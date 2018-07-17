d103f63f68cb226af3e2ba3fe3ef3fef1b4a5e2a
Giouzakov Kostas <giouzakovkostas@gmail.com>
2018-07-17 18:06:02 +0300

    Adding NgForms

--- Angular Fundamentals/AngularApp/src/app/user/user.module.ts
+++ Angular Fundamentals/AngularApp/src/app/user/user.module.ts
@@ -1,17 +1,22 @@
 import { NgModule } from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {RouterModule} from '@angular/router';
+import {FormsModule} from '@angular/forms'
+
 import { ProfileComponent } from './profile.component';
 import { UserRoutes } from './user.routes';
+import { LoginComponent } from './login/login.component';
 
 
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
     RouterModule.forChild(UserRoutes)
   ],
   declarations: [
-    ProfileComponent
+    ProfileComponent,
+    LoginComponent
   ],
   providers: []
 })
